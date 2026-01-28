917b84568f88655cf96faea6f7f1ae1fed152133
vikas <vikashsharma2523@gmail.com>
2026-01-28 20:16:34 +0530

    Initial commit

--- src/data/contactsData.js
+++ src/data/contactsData.js
@@ -1,7 +1,7 @@
 export const contactsData = {
     email: 'vikashshaarmaa@gmail.com',
     phone: '+918851623906',
-    address: 'Raraul Baghpat, Uttar pardesh, India - 250101 ',
+    address: 'Rataul Baghpat, Uttar pardesh, India - 250101 ',
 
     sheetAPI: ''
 }
